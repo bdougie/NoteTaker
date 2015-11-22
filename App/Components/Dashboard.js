36608bce1dc1c7cef1a94b62bf84aa14012b8d60
Brian Douglas <ilikerobot@gmail.com>
2015-11-21 23:48:06 -0800

    adds route buttons and image

--- App/Components/Dashboard.js
+++ App/Components/Dashboard.js
@@ -4,6 +4,8 @@ let {
   Text,
   View,
   StyleSheet,
+  Image,
+  TouchableHighlight,
 } = React;
 
 let styles = StyleSheet.create({
@@ -22,11 +24,54 @@ let styles = StyleSheet.create({
 });
 
 export default class Dashboard extends React.Component{
+  makeBackground(btn){
+    var obj = {
+      flexDirection: 'row',
+      alignSelf: 'stretch',
+      justifyContent: 'center',
+      flex: 1
+    }
+    if(btn === 0){
+      obj.backgroundColor = '#48BBEC';
+    } else if (btn === 1){
+      obj.backgroundColor = '#E77AAE';
+    } else {
+      obj.backgroundColor = '#758BF4';
+    }
+    return obj;
+  }
+  goToProfile(){
+    console.log('Going to Profile Page');
+  }
+  goToRepos(){
+    console.log('Going to Repos');
+  }
+  goToNotes(){
+  console.log('Going to Notes');
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text> This is the dashboard </Text>
-        <Text> {this.props.userInfo} </Text>
+        <Image source={{uri: this.props.userInfo.avatar_url}} style={styles.image}/>
+        <TouchableHighlight
+          style={this.makeBackground(0)}
+          onPress={this.goToProfile.bind(this)}
+          underlayColor='#88D4F5'>
+            <Text style={styles.buttonText}>View Profile</Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+            style={this.makeBackground(1)}
+            onPress={this.goToRepos.bind(this)}
+            underlayColor="#E39EBF">
+              <Text style={styles.buttonText}>View Repositories</Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+            style={this.makeBackground(2)}
+            onPress={this.goToNotes.bind(this)}
+            underlayColor="#9BAAF3">
+              <Text style={styles.buttonText}>Take Notes</Text>
+        </TouchableHighlight>
       </View>
     )
   }
