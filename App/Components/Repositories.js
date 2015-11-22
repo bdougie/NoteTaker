2308fd7c8e07eaa76f849a2eaf899a05f026fcde
Brian Douglas <ilikerobot@gmail.com>
2015-11-22 00:24:16 -0800

    adds repo route and class

---
+++ App/Components/Repositories.js
@@ -0,0 +1,73 @@
+import React from 'react-native';
+import Badge from './Badge';
+import Separator from './Helpers/Separator';
+
+let {
+  ScrollView,
+  Text,
+  View,
+  TouchableHighlight,
+  StyleSheet
+} = React;
+
+let styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  rowContainer: {
+    flexDirection: 'column',
+    flex: 1,
+    padding: 10
+  },
+  name: {
+    color: '#48BBEC',
+    fontSize: 18,
+    paddingBottom: 5
+  },
+  stars: {
+    color: '#48BBEC',
+    fontSize: 14,
+    paddingBottom: 5
+  },
+  description: {
+    fontSize: 14,
+    paddingBottom: 5
+  }
+});
+
+export default class Repositories extends React.Component{
+  openPage(url){
+    console.log('the url is', url);
+    }
+  render(){
+    let repos = this.props.repos;
+    let list = repos.map((item, index) => {
+      let desc = repos[index].description ? <Text style={styles.description}> {repos[index].description} </Text> : <View />;
+      return (
+        <View key={index}>
+          <View style={styles.rowContainer}>
+            <TouchableHighlight
+              onPress={this.openPage.bind(this, repos[index].html_url)}
+              underlayColor='transparent'>
+              <Text style={styles.name}>{repos[index].name}</Text>
+            </TouchableHighlight>
+            <Text style={styles.stars}> Stars: {repos[index].stargazers_count} </Text>
+            {desc}
+          </View>
+          <Separator />
+        </View>
+      )
+    });
+    return (
+      <ScrollView style={styles.container}>
+        <Badge userInfo={this.props.userInfo} />
+        {list}
+      </ScrollView>
+    )
+  }
+};
+
+Repositories.propTypes = {
+  userInfo: React.PropTypes.object.isRequired,
+  repos: React.PropTypes.array.isRequired
+}
